3afd2aa02dd2088a4ad17c565460f99e95cfff41
Brittany Jungclaus <164208757+NovaLanceBrittany@users.noreply.github.com>
2024-05-29 01:09:59 -0500

    finished video record, and finished README

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
-
+// Requirinig in Mongoose and MongoDB
 
 const { connect, connection } = require('mongoose');
-
 const connectionString = 'mongodb://127.0.0.1:27017/socialMediaApiDB';
 
 connect(connectionString);
